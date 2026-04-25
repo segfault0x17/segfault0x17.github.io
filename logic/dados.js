c1a80a43f0cdd57ac3d22680de38aa14248eb813
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2026-04-25 15:42:35 -0300

    fix: remoção das tags de paragrafo

--- logic/dados.js
+++ logic/dados.js
@@ -1,17 +1,17 @@
 export const artigos = {
   "fragment_000": {
     title: "Introdução",
-    date: "19-04-2026",
+    date: "25-04-2026",
     content: `
-      <p>Nesse espaço irei usar para postar conteúdos mais completos que ficaria extenso para o instagram.<br>
-Então fiquem ligados que mais coisas legais virão.</p>
+Nesse espaço irei usar para postar conteúdos mais completos que ficaria extenso para o instagram.
+Então fiquem ligados que mais coisas legais virão. Meu foco será binary exploitation, reverse engineering e forensics.
     `
   },
   "fragment_002": {
     title: "Ofuscação por Xor e como ela se comporta em um debugger",
     date: "date not knowing",
     content: `
-      <p>Volte mais tarde. Coisas serão contruídas</p>
+Volte mais tarde. Coisas serão contruídas
     `
   }
 };
