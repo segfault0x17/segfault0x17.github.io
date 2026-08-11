254652d502a6d56fbab20806a0ab27065d9ff18b
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2026-08-11 19:16:28 -0300

    add: try hack me and hack the box profile links add: overflow-wrap in smartphone screens

--- logic/dados.js
+++ logic/dados.js
@@ -6,7 +6,7 @@ export const artigos = {
     content: `
 Pesquisador independente em segurança ofensiva, com interesse em técnicas de anti-debugging, engenharia reversa e 
 análise de binários Windows. Atualmente desenvolvendo estudos e projetos pessoais voltados à compreensão de mecanismos anti-análise e funcionamento interno de aplicações em baixo nível.
-<br>Nesse espaço irei usar para postar conteúdos mais completos que ficaria extenso para o <a href="https://instagram.com/segfault0x17">instagram</a>.
+<br>Nesse espaço irei usar para postar conteúdos mais completos que ficaria extenso para o <a href="https://instagram.com/ewerton.sh">instagram</a>, também <a href="https://tryhackme.com/p/proxyy">TryHackMe</a> e <a href="https://profile.hackthebox.com/profile/019d3531-ede3-72db-a467-32242c01683c">Hack The Box</a>.
 <style>
     a {text-decoration: underline}
 </style>
