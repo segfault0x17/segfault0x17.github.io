ca197bda345a09ddee10782198ff826ed017922b
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2026-09-08 01:22:19 -0300

    add: dict categorie web

--- logic/script.js
+++ logic/script.js
@@ -75,7 +75,8 @@ async function loadContent(id) {
       'sys': 'System Internals',
       'lab': 'Code Experiments',
       'tool': 'Utilitaries',
-      'int': 'Introduction'
+      'int': 'Introduction',
+      'web': 'Web Security'
     };
 
     // Pega a sigla que veio do artigo (ex: "rev"), converte para minúsculo 
